38a18155dd829990b407e27028ec80cee84a4667
gholapksh <iragholap2@gmail.com>
2025-10-15 22:23:41 -0500

    initial commit

--- amazon-best-sellers-analysis/main.py
+++ amazon-best-sellers-analysis/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #Rest of the file go here
 
-df = pd.read_csv(bestsellers.csv)
+df = pd.read_csv("/Users/IraGholap/Amazon-Books/amazon-best-sellers-analysis/bestsellers.csv")
 
 #Get the first 5 rows of the spreadsheet
 print(df.head())
@@ -15,3 +15,21 @@ print(df.columns)
 
 #Get summary statistics for each column
 print(df.describe())
+
+df.drop_duplicates(inplace=True)
+
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+
+df["Price"] = df["Price"].astype(float)
+
+author_counts = df['Author'].value_counts()
+print(author_counts)
+
+avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
+print(avg_rating_by_genre)
+
+# Export top selling authors to a CSV file
+author_counts.head(10).to_csv("top_authors.csv")
+
+# Export average rating by genre to a CSV file
+avg_rating_by_genre.to_csv("avg_rating_by_genre.csv")
